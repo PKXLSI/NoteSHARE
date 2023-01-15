7aaf06dabf1002e35f8af64b858ccbf1f4bcc342
SunnyY15926 <45080122+SunnyY15926@users.noreply.github.com>
2023-01-15 02:54:42 -0500

    Worked on authentication
    tried some methods for implementing user logins

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {getStorage} from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -15,6 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 const analytics = getAnalytics(app);
